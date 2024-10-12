5910fec8cc98ee8654616e80bc21e5c8d7f78d7f
Michael <121832548+GhostPrime1234@users.noreply.github.com>
2024-10-12 11:28:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ def password_checker(password: str) -> bool:
     schema = PasswordValidator()
 
     # Add properties to it
-    schema.min(8)
-    schema.max(30)
-    schema.has().uppercase()
-    schema.has().lowercase()
-    schema.has().digits()
-    schema.has().no().spaces()
+    schema.min(8).max(30)       # Minimum and maximum length
+    schema.has().uppercase()    # At least one uppercase letter
+    schema.has().lowercase()    # At least one lowercase letter
+    schema.has().digits()       # At least one digit
+    schema.has().no().spaces()  # No spaces allowed
+    schema.has().symbols()      # At least on special character
+
     return schema.validate(password)
