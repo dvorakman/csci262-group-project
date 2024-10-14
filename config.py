02e9da7a77781b5e7545c6a3c6d16dc56789fb56
Derek <78566663+dvorakman@users.noreply.github.com>
2024-10-14 05:18:40 +0000

    Refactor config.py to use environment variables for SECRET_KEY and PEPPER or default

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 from os import urandom
 
 class Config:
-    SECRET_KEY = 'superduperultrasecretmegabananzatopsecretclearanceonlykey'
-    PEPPER = urandom(16)
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super_duper_ultra_secret_default_key')
+    PEPPER = os.environ.get('PEPPER', urandom(16))
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_SAMESITE = 'Lax'
